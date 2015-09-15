be2d0afcc9dcc980ac26d4cc5203ce8a9b7aba6d
Tony Stewart <tony.stewart@tcc-tstewart.local>
2015-09-15 09:43:20 -0500

    Initial Commit

---
+++ Tip Calculator 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator 2
+//
+//  Created by Tony Stewart on 9/15/15.
+//  Copyright (c) 2015 Tony Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
